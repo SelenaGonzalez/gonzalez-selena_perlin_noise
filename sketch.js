e26aedeb4b9dc2787849ab3e8f6c2b3ee2f47a28
SelenaGonzalez <selena.gonzalez@ucuenca.edu.ec>
2023-10-26 16:30:13 -0500

    se combino el perlin noise y el sonido en una pelotita

--- sketch.js
+++ sketch.js
@@ -1,28 +1,59 @@
 let noiseTime = 0;
 let noiseTam = 25;
+let osc, playing, freq, amp;
+let t = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0);
+  osc = new p5.Oscillator("sawtooth");
 }
 
 function draw() {
-  let posX1 = map(noise(noiseTime + 0.008), 0, 1, 0, windowWidth);
+  let posX1 = map(noise(noiseTime), 0, 1, 0, windowWidth);
   let posY1 = map(noise(noiseTime + 100), 0, 1, 0, windowHeight);
 
-  let posX2 = map(noise(noiseTime), 0, 1, windowWidth, 0);
-  let posY2 = map(noise(noiseTime + 100), 0, 1, windowHeight, 0);
+  //let posX2 = map(noise(noiseTime + 0.005), 0, 1, windowWidth, 0);
+  //let posY2 = map(noise(noiseTime + 100), 0, 1, windowHeight, 0);
+
+  //let posX3 = map(noise(noiseTime + 0.001), 0, 1, 0, windowWidth);
+  //let posY3 = map(noise(noiseTime + 100), 0, 1, 0, windowHeight);
+
+  //let posX4 = map(noise(noiseTime + 30), 0, 1, windowWidth, 0);
+  //let posY4 = map(noise(noiseTime + 200), 0, 1, windowHeight, 0);
 
   let tam = map(noise(noiseTam), 0, 1, 50, 50);
 
   noiseTime += 0.006;
   noiseTam += 0.007;
 
-  fill(255, random(10, 100), 200, random(100, 200), 20);
+  fill(255, 70, 255);
   stroke(0, 0, 20, 50);
   strokeWeight(20);
   rect(posX1, posY1, tam);
-  circle(posX2, posY2, tam);
+
+  //fill(100, 255, 100);
+  //stroke(0, 0, 20, 100);
+  //strokeWeight(10);
+  //circle(posX2, posY2, tam);
+
+  //fill(255, 70, 255);
+  //stroke(0, 0, 20, 50);
+  //strokeWeight(20);
+  //rect(posX3, posY3, tam);
+
+  //fill(100, 255, 100);
+  //stroke(0, 0, 20, 100);
+  //strokeWeight(10);
+  //circle(posX4, posY4, tam);
 
   // noStroke(0);
+
+  osc.start();
+  freq = constrain(map(noise(noiseTime), 0, 1, 500, 900), 500, 900);
+  amp = 0.5;
+
+  osc.freq(freq, 0.1);
+  osc.amp(amp, 0.1);
+  t += 0.01;
 }
