591643e51f80b86c66e60cba13615dc8058757f9
SelenaGonzalez <selena.gonzalez@ucuenca.edu.ec>
2023-10-27 22:35:06 -0500

    Proyecto  con Perlin Noise experimentación color

--- classes/Figura.js
+++ classes/Figura.js
@@ -1,7 +1,7 @@
 class Figura {
   constructor() {
     this.noiseTime = 0;
-    this.noiseTam = 50;
+    this.noiseTam = 100;
     // this.osc, playing, freq, amp;
     // this.t = 0;
   }
@@ -16,18 +16,27 @@ class Figura {
 
     this.noiseTime += 0.005;
     this.noiseTam += 0.008;
+
+    this.r = 60 * this.noiseTime;
+    this.g = 100 * this.noiseTime;
+    this.b = 30 * this.noiseTime;
+    this.r2 = 80 * this.noiseTime;
+    this.g2 = 20 * this.noiseTime;
+    this.b2 = 150 * this.noiseTime;
   }
 
   display() {
-    fill(255, 70, 255);
-    stroke(211, 169, 250);
-    strokeWeight(0.5);
+    fill(this.r, this.g, this.b);
+    // noStroke();
+    // stroke(211, 169, 250);
+    strokeWeight(0.09);
     rect(this.posX1, this.posY1, this.tam);
 
-    fill(150, 255, 100);
-    stroke(50, 75, 20);
-    strokeWeight(1);
-    circle(this.posX2, this.posY2, this.tam);
+    fill(this.r2, this.g2, this.b2);
+    // noStroke();
+    // stroke(50, 75, 20);
+    strokeWeight(0.05);
+    circle(this.posX2, this.posY2, this.tam + 20);
 
     // osc.start();
     // freq = constrain(map(noise(this.noiseTime), 0, 1, 300, 100), 100, 300);
